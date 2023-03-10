b8ef0ca90d221633114061a7830063632a0d3e12
Ana Beatriz Adriano da Silva <ana.github0@gmail.com>
2023-03-10 11:45:22 -0300

    Os components About e Interests estão prontos

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,11 +1,15 @@
 import "./Card.css";
 
 import Information from "./Information/Information";
+import About from "./About/About";
+import Interest from "./Interests/Interests";
 
 export default function Card() {
   return (
     <div className="card">
       <Information />
+      <About /> 
+      <Interest /> 
     </div>
   )
 }
---
+++ src/Components/Interests/Interests.js
@@ -0,0 +1,10 @@
+import "./interest.css";
+
+export default function interest() {
+  return (
+    <div className="interests">
+      <h2>interest</h2>
+      <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+    </div>
+  )
+}
